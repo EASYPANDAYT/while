d10b6e08d90c47d5409e2385f1f8fab11fce3f50
EASYPANDAYT <145465554+EASYPANDAYT@users.noreply.github.com>
2023-10-03 17:26:40 +0300

    Add files via upload

--- while.py
+++ while.py
@@ -19,15 +19,17 @@ while way_left == False and way_center == False and way_right == False:
     way = input ('В какую сторону поехать? ')
     if way == 'налево' :
         if way_left == False :
+        elif way_left == True :
+                print(name, 'уже был на левой дороге')
             print (name, 'вышел на разбойников')
             choose = input ('Что делать? биться или бежать? ')
             if choose == 'биться':
                 print (name, 'раскидал всех разбойников и стал их предводителем')
             elif choose == 'бежать':
                 print (name, 'убежал от разбойников назад')
+                way_left == True
+                continue
             way_left == True 
-        else :
-            print (name, 'уже был на левой дороге.')
     elif way == 'направо':
         if way_right == False :
             print (name, 'богат')
